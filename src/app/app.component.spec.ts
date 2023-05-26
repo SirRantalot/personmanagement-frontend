6c67d5f4fad8f3842ccb5611fb32f397585591a1
SirRantalot <richard.hargrave@bbzbl-it.ch>
2023-05-26 15:51:50 +0200

    removed default test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -28,12 +28,6 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'personmanagement-frontend'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('personmanagement-frontend');
-  });
-
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
