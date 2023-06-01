57031aa1bde2e3004c9aa2e55eb770c762e34736
SirRantalot <richard.hargrave@bbzbl-it.ch>
2023-06-01 16:15:38 +0200

    Contact Details create

--- src/app/pages/contact-details-create/contact-details-create.component.ts
+++ src/app/pages/contact-details-create/contact-details-create.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { UntypedFormGroup, UntypedFormControl, UntypedFormBuilder } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router, ActivatedRoute } from '@angular/router';
+import { ContactDetails } from 'src/app/dataaccess/contact-details';
+import { ContactDetailsService } from 'src/app/service/contact-details.service';
 
 @Component({
   selector: 'app-contact-details-create',
@@ -7,4 +12,56 @@ import { Component } from '@angular/core';
 })
 export class ContactDetailsCreateComponent {
 
+  contactDetails = new ContactDetails();
+  public objForm = new UntypedFormGroup({
+    firstname: new UntypedFormControl(''),
+    name: new UntypedFormControl('')
+  });
+
+  constructor(private router: Router, private route: ActivatedRoute,
+              private snackBar: MatSnackBar, private formBuilder: UntypedFormBuilder,
+              private contactDetailsService: ContactDetailsService) {}
+
+  ngOnInit(): void {
+    if (this.route.snapshot.paramMap.get('id') !== null) {
+      const id = Number.parseInt(this.route.snapshot.paramMap.get('id') as string);
+
+      this.contactDetailsService.getOne(id).subscribe(obj => {
+        this.contactDetails = obj;
+        this.objForm = this.formBuilder.group(obj);
+      });
+    } else {
+      this.objForm = this.formBuilder.group(this.contactDetails);
+    }
+  }
+
+  async back() {
+    await this.router.navigate(['contactdetails']);
+  }
+
+  async save(formData: any) {
+    this.contactDetails = Object.assign(formData);
+
+    if (this.contactDetails.id) {
+      this.contactDetailsService.update(this.contactDetails).subscribe({
+        next: () => {
+          this.snackBar.open('ContactDetails saved', 'Close', {duration: 5000});
+          this.back();
+        },
+        error: () => {
+          this.snackBar.open('Failed to save contactDetails', 'Close', {duration: 5000, politeness: 'assertive'});
+        }
+      });
+    } else {
+      this.contactDetailsService.save(this.contactDetails).subscribe({
+        next: () => {
+          this.snackBar.open('New contactDetails saved', 'Close', {duration: 5000});
+          this.back();
+        },
+        error: () => {
+          this.snackBar.open('Failed to save new contactDetails', 'Close', {duration: 5000, politeness: 'assertive'});
+        }
+      });
+    }
+  }
 }
