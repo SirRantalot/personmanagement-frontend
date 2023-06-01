2b1b088aa4a9029f3b8ff78c0c4691678f0cf534
SirRantalot <richard.hargrave@bbzbl-it.ch>
2023-06-01 09:45:24 +0200

    Fixed project setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { AppComponent } from './app.component';
 
 const routes: Routes = [
   {path: '', component: DashboardComponent}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { AppAuthService } from './service/app.auth.service';
 import {OAuthService} from 'angular-oauth2-oidc';
+import {Subscription} from "rxjs";
+import {MenuService} from "./service/menu.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
+  currentPage = '';
+  private subPage?: Subscription;
 
-  constructor(private authService: AppAuthService, public oauthService: OAuthService) {}
+  constructor(private authService: AppAuthService, public oauthService: OAuthService, private menuService: MenuService) {}
 
   public login() {
     this.authService.login();
@@ -18,4 +22,19 @@ export class AppComponent {
   public logout() {
     this.authService.logout();
   }
+
+  async ngOnInit() {
+    this.subPage = this.menuService.pageObservable.subscribe(page => {
+      this.currentPage = page;
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.subPage?.unsubscribe();
+  }
+
+  async navigateTo(page: string) {
+    await this.menuService.setPage(page);
+  }
 }
+
